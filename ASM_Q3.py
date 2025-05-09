0f236de193e483ac8d4cf257191bda2c09850db7
lot nikee <lotnikee@github.com>
2025-05-09 19:02:38 +0100

    commit

--- ASM_Q3.py
+++ ASM_Q3.py
@@ -45,13 +45,13 @@ CO_transition_energy = -2.29
 CO_oxidation_energy = 0.75
 
 ### Define a function to calculate the 2D translational partition function value 
-def trans_2D(species_mass, boltzmann_J, temperature, h_J, A):
+def trans_2D(species_mass):
     return ((A * np.pi * species_mass * boltzmann_J * temperature) / (h_J ** 2))
 
 ### Call the different 2D translational values 
-trans_CO = trans_2D(m_CO, boltzmann_J, temperature, h_J, A)
-trans_O2 = trans_2D(m_O2, boltzmann_J, temperature, h_J, A)
-trans_CO2 = trans_2D(m_CO2, boltzmann_J, temperature, h_J, A)
+trans_CO = trans_2D(m_CO)
+trans_O2 = trans_2D(m_O2)
+trans_CO2 = trans_2D(m_CO2)
 
 ### Define a function for the calculation of the vibrational partition function values for the different species 
 def vibrational_partitions(vib_species, temperature, h_J, c, boltzmann_J):
@@ -69,31 +69,60 @@ vib_O2_dissociation = vibrational_partitions(O2_dissociation, temperature, h_J, 
 vib_CO_oxidation = vibrational_partitions(CO_oxidation, temperature, h_J, c, boltzmann_J)
 
 ### Define a function to determine the CO adsorption reaction rate
-def CO_ads(boltzmann_J, temperature, P_CO, A, m_CO):
+def CO_ads():
     CO_ads_numerator = A * P_CO
     CO_ads_denominator = np.sqrt(2 * np.pi * m_CO * boltzmann_J * temperature)
     return CO_ads_numerator / CO_ads_denominator
 
 ### Define a function to determine the CO desorption reaction rate
-def CO_des(boltzmann_J, temperature, h_J, vib_CO, rot_CO, boltzmann_ev, CO_adsorption_energy, trans_CO):
+def CO_des():
     CO_des_numerator = (boltzmann_J * temperature) * ((vib_CO * rot_CO * trans_CO) / vib_CO_surface) * np.exp((CO_adsorption_energy / (boltzmann_ev * temperature)))
     CO_des_denominator = (h_J * vib_CO_surface)
     return CO_des_numerator / CO_des_denominator
 
 ### Define a function to determine the O2 adsorption reaction rate 
-def O2_ads(vib_O2_dissociation, vib_O2, rot_O2, trans_O2, boltzmann_ev, A, P_O2, m_O2, temperature, boltzmann_J):
+def O2_ads():
     O2_ads_numerator = (vib_O2_dissociation * 2 * A * P_O2 * np.exp(-O2_transition_energy / (boltzmann_ev * temperature)))
-    O2_ads_denominator = vib_O2 * rot_O2 * trans_O2 * np.sqrt(2 * np.pi * m_O2 * boltzmann_J * temperature)
+    O2_ads_denominator = 2 * vib_O2 * rot_O2 * trans_O2 * np.sqrt(2 * np.pi * m_O2 * boltzmann_J * temperature)
     return O2_ads_numerator / O2_ads_denominator
 
-###
+### Define a function to determine the O2 desorption reaction rate 
+def O2_des():
+    O2_des_numerator = boltzmann_J * temperature * vib_O2_dissociation * np.exp((-O2_transition_energy - O2_adsorption_energy) / (boltzmann_ev * temperature))
+    O2_des_denominator = h_J * (vib_O_surface ** 2)
+    return O2_des_numerator / O2_des_denominator
 
+### Define a function to determine the CO oxidation reaction rate
+def CO_oxi():
+    CO_oxi_numerator = boltzmann_J * temperature * vib_CO_oxidation * np.exp((-CO_transition_energy) / (boltzmann_ev * temperature))
+    CO_oxi_denominator = h_J * vib_CO_surface * vib_O_surface
+    return CO_oxi_numerator / CO_oxi_denominator
 
+### Define a function to determine the CO reversed oxidation reaction rate
+def CO_oxi_rev():
+    CO_oxi_rev_numerator = vib_CO_oxidation * 2 * A * P_CO * np.exp((-CO_transition_energy - CO_oxidation_energy) / (boltzmann_ev * temperature))
+    CO_oxi_rev_denominator = 2 * vib_CO2 * rot_CO2 * trans_CO2 * np.sqrt(2 * np.pi * m_CO2 * boltzmann_J * temperature)
+    return CO_oxi_rev_numerator / CO_oxi_rev_denominator
 
+### Call all the reaction rates 
+k_CO_ads = CO_ads()
+k_CO_des = CO_des()
+k_O2_ads = O2_ads()
+k_O2_des = O2_des()
+k_CO_oxi = CO_oxi()
+k_CO_oxi_rev = CO_oxi_rev()
 
+### Print all the reaction rate constants 
+print("CO adsorption rate constant      = ", k_CO_ads)
+print("CO desorption rate constant      = ", k_CO_des, "\n")
+print("O2 adsorption rate constant      = ", k_O2_ads)
+print("O2 desorption rate constant      = ", k_O2_des, "\n")
+print("CO oxidation rate constant      = ", k_CO_oxi)
+print("CO reverse oxidation rate constant      = ", k_CO_oxi_rev, "\n")
 
+### Determine final expression
+rate_constant = ((k_O2_ads / k_O2_des) ** 0.5) * (k_CO_ads / k_CO_des) * (k_CO_oxi / k_CO_oxi_rev)
 
-
-
-
+### Print final expression 
+print("Final reaction rate expresion   =", rate_constant)
 
