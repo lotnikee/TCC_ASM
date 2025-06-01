11d9d3d05986231533da0860442f209e11ff97be
lot nikee <lotnikee@github.com>
2025-06-01 11:55:49 +0100

    commit

--- ASM_Q2.py
+++ ASM_Q2.py
@@ -1,14 +1,17 @@
 import numpy as np 
 
-### Vibrational frequencies constants (cm^-1)
-vib_freq_O2 = 1580
-vib_freq_CO = 2143
-vib_freq_CO2 = [2349, 1388, 667, 667]
+### eV to Joules conversion rate 
+conversion = 1.602177e-19
 
-### Moments of inertia constants (eV s^2)
-O2_inertia = 1.22e-27
-CO_inertia = 9.09e-28
-CO2_inertia = 4.46e-27
+### Vibrational frequencies constants (m^-1)
+vib_freq_O2 = 158000
+vib_freq_CO = 214300
+vib_freq_CO2 = [234900, 138800, 66700, 66700]
+
+### Moments of inertia constants (J s^2)
+O2_inertia = 1.22e-27 * conversion
+CO_inertia = 9.09e-28 * conversion
+CO2_inertia = 4.46e-27 * conversion
 
 ### Symmetry numbers 
 O2_symmetry = 2
@@ -20,42 +23,49 @@ m_O2 = 5.3137e-26
 m_CO = 4.6495e-26
 m_CO2 = 7.3068e-26
 
+### Initial energies (J)
+initial_energy_CO2 = -2.77 * conversion
+initial_energy_CO = 0 
+initial_energy_O2 = 0
+
+### Partial pressures (Pa)
+p_CO = 6.0e-04
+p_O2 = 1.2e-03
+
 ### Other constants 
-boltzmann_ev = 8.617e-05    ### eV/K
-temperature = 900           ### K
-c = 2.998e+10               ### m/s
-h_ev = 4.1357e-15           ### eV*s
-pressure = 0.30             ### bar
-partial_CO = 6.0e-09        ### bar 
-partial_O2 = 1.2e-08        ### bar
-h_bar_ev = 6.582e-16        ### eV*s
+boltzmann_joules = 1.380649e-23     ### J/K
+boltzmann_ev = 8.617333e-05         ### eV/K
+temperature = 900                   ### K
+c = 2.998e+08                       ### m/s
+h_ev = 4.135668e-15                 ### eV*s
+h_joules = 6.626070e-34             ### J*s 
+h_bar_ev = 6.582120e-16             ### eV*s
+h_bar_joules = 1.054572e-34         ### J*s
 
 ### Energy exponent 
-energy_exponent = np.exp(-(-2.77) / (boltzmann_ev * temperature))
+energy_exponent = np.exp((-initial_energy_CO2 - initial_energy_CO - (0.5 * initial_energy_O2)) / (boltzmann_joules * temperature))
 
 ### Calculation of the mass contribution 
-mass_contribution = (m_CO2 ** 1.5) / ((m_CO ** 1.5) * (m_O2 ** 0.75))
+mass_contribution = (m_CO2 / ((m_O2 ** 0.5) * m_CO)) ** (3/2)
 
 ### Calculate the constant contributions 
-constants_pressure = ((boltzmann_ev * temperature) / pressure) ** (-0.5)
-constants_h = ((2 * np.pi * boltzmann_ev * temperature) / (h_ev ** 2)) ** (-0.75)
-constants_contribution = constants_pressure * constants_h
+constants = ((h_joules ** 3) / ((2 * np.pi ** (3/2)) * (boltzmann_joules * temperature ** (5/2))))** (1/2)
 
 ### Calculate the vibrational partition function values for the different species 
-def vibrational_partitions(temperature, h_ev, c, boltzmann_ev):
+def vibrational_partitions(temperature, h_joules, c, boltzmann_joules):
     vib_CO2_list = []
     for freq in vib_freq_CO2:
-        exponent = (-c * h_ev * freq) / (boltzmann_ev * temperature)
-        q_vib_CO2 = 1 / (1 - np.exp(exponent))
+        exponent = (-c * h_joules * freq) / (boltzmann_joules * temperature)
+        q_vib_CO2 = np.exp((-0.5 * c * h_joules * freq) / (boltzmann_joules * temperature)) / (1 - np.exp(exponent))
         vib_CO2_list.append(q_vib_CO2)
     return np.prod(vib_CO2_list)
 
-q_vib_CO2 = vibrational_partitions(temperature, h_ev, c, boltzmann_ev)
-q_vib_CO = 1 / (1 - np.exp((-c * h_ev * vib_freq_CO) / (boltzmann_ev * temperature)))
-q_vib_O2 = 1 / (1 - np.exp((-c * h_ev * vib_freq_O2) / (boltzmann_ev * temperature)))
+q_vib_CO2 = vibrational_partitions(temperature, h_joules, c, boltzmann_joules)
+q_vib_CO = np.exp((-0.5 * c * h_joules * vib_freq_CO) / (boltzmann_joules * temperature)) / (1 - np.exp((-c * h_joules * vib_freq_CO) / (boltzmann_joules * temperature)))
+q_vib_O2 = np.exp((-0.5 * c * h_joules * vib_freq_O2) / (boltzmann_joules * temperature)) / (1 - np.exp((-c * h_joules * vib_freq_O2) / (boltzmann_joules * temperature)))
 
 ### Calculate the rotational partition function values for the different species 
-rotational_constant = (2 * boltzmann_ev * temperature) / (h_bar_ev ** 2)
+rotational_constant = (2 * boltzmann_joules * temperature) / (h_bar_joules ** 2)
 q_rot_CO = rotational_constant * (CO_inertia / CO_symmetry)
 q_rot_O2 = rotational_constant * (O2_inertia / O2_symmetry)
 q_rot_CO2 = rotational_constant * (CO2_inertia / CO2_symmetry)
@@ -66,12 +76,12 @@ rot_vib_contribution_denominator = (q_vib_CO * q_rot_CO) * ((q_rot_O2 * q_vib_O2
 rot_vib_contribution = rot_vib_contribution_numerator / rot_vib_contribution_denominator
 
 ### Calculation of the CO2 partial pressure 
-partial_CO2 = constants_contribution * mass_contribution * rot_vib_contribution * partial_CO * np.sqrt(partial_O2)
+p_CO2 = np.sqrt(constants * mass_contribution * rot_vib_contribution * p_CO * (p_O2 ** 1/2))
 
 ### Print calculation values to check if they are correct
 print(f'Energy exponent = {energy_exponent:.5g}')
 print(f'Mass contribution = {mass_contribution:.5g}')
-print(f'Constants contribution = {constants_contribution:.5g}'"\n")
+print(f'Constants contribution = {constants:.5g}'"\n")
 
 print(f"Vibrational partition function of CO2: {q_vib_CO2:.5g}")
 print(f'Vibrational partition function value CO = {q_vib_CO:.5g}')
@@ -82,5 +92,4 @@ print(f'Rotational partition function value CO = {q_rot_CO:.5g}')
 print(f'Rotational partition function value O2 = {q_rot_O2:.5g}'"\n")
 
 print(f'Rotational and vibrational contributions = {rot_vib_contribution:.5g}')
-print(f'Partial pressure CO2 = {partial_CO2:.5g}')
-
+print(f'Partial pressure CO2 = {p_CO2:.5g}')
